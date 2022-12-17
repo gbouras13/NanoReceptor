1d5869f7c0346c8b730bd2525c25af625870028d
gbouras13 <george.bouras@adelaide.edu.au>
2022-12-17 14:10:57 +1030

    modules

--- nanoreceptorModules/main.py
+++ nanoreceptorModules/main.py
@@ -1,30 +1,27 @@
 from audioop import ratecv
 import os
 import sys
-# from input_commands import get_input, validate_fastq, instantiate_dirs
-# from processes import run_minimap2, keep_primary_supplementary_mappings_convert_sam, keep_primary_supplementary_mappings_convert_sam_direct, run_flagstat
-# from post_processing import get_total_read_count, parse_bam, pivot_df
-
+import nanoreceptorModules
 
 DBDIR = os.path.join(os.path.dirname(__file__),'../',"databases/")  
 
 def main(argv):  
 
-    args = get_input()
-    validate_fastq(args.infile)
-    out_dir = instantiate_dirs(args.outdir) # incase there is already an outdir
-    run_minimap2(args.infile, out_dir, DBDIR, args.prefix, args.species)
+    args = nanoreceptorModules.get_input()
+    nanoreceptorModules.validate_fastq(args.infile)
+    out_dir = nanoreceptorModules.instantiate_dirs(args.outdir) # incase there is already an outdir
+    nanoreceptorModules.run_minimap2(args.infile, out_dir, DBDIR, args.prefix, args.species)
     if args.kit == "cdna":
-        keep_primary_supplementary_mappings_convert_sam(out_dir, args.prefix)
+        nanoreceptorModules.keep_primary_supplementary_mappings_convert_sam(out_dir, args.prefix)
     elif args.kit == "direct":
-        keep_primary_supplementary_mappings_convert_sam_direct(out_dir, args.prefix)
-    run_flagstat(out_dir, args.prefix)
+        nanoreceptorModules.keep_primary_supplementary_mappings_convert_sam_direct(out_dir, args.prefix)
+    nanoreceptorModules.run_flagstat(out_dir, args.prefix)
     # post processing
-    total_read_count = get_total_read_count(out_dir,args.prefix)
-    match_df = parse_bam(out_dir, args.prefix)
-    pivot_df(out_dir, match_df, total_read_count, args.prefix, args.kit, args.species)
+    total_read_count = nanoreceptorModules.get_total_read_count(out_dir,args.prefix)
+    match_df = nanoreceptorModules.parse_bam(out_dir, args.prefix)
+    nanoreceptorModules.pivot_df(out_dir, match_df, total_read_count, args.prefix, args.kit, args.species)
    
-    sys.exit("nanoreceptor has finished")  
+    print("nanoreceptor has finished")
 
 
 def run():
