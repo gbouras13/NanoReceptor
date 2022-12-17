5e151baf3be0126a8747a48412b6b2bc08d0c245
gbouras13 <george.bouras@adelaide.edu.au>
2022-12-17 12:52:16 +1030

    fix build

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,8 @@
 python3 -m pip install . --no-deps -vv 
 
 mkdir -p "${PREFIX}/bin"
+mkdir -p "${PREFIX}/bin/nanoreceptorModules"
 
-cp -r nanoreceptorModules/* $PREFIX/bin/
+cp -r nanoreceptor.py $PREFIX/bin/
+cp -r nanoreceptorModules/* $PREFIX/bin/nanoreceptorModules/
 
