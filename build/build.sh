967d27709a5f12fa995b5103d3f4de42c770e547
gbouras13 <george.bouras@adelaide.edu.au>
2022-12-18 10:49:37 +1030

    change build for databases

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,9 @@ python3 -m pip install . --no-deps -vv
 
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PREFIX}/bin/nanoreceptorModules"
+mkdir -p "${PREFIX}/bin/databases"
 
 cp -r nanoreceptor.py $PREFIX/bin/
 cp -r nanoreceptorModules/* $PREFIX/bin/nanoreceptorModules/
+cp -r databases/* $PREFIX/bin/databases/
 
